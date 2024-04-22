8158225269713573f377e3ebeb5965ea0e26bd41
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-22 22:08:22 +0200

    Create ClientDAO.java

---
+++ src/main/java/team5/BW5/repositories/ClientDAO.java
@@ -0,0 +1,11 @@
+package team5.BW5.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team5.BW5.entities.Client;
+
+import java.util.Optional;
+
+public interface ClientDAO extends JpaRepository<Client,Long> {
+    Optional<Client> findById(Long id);
+    Optional<Client>findByEmail(String email);
+}
