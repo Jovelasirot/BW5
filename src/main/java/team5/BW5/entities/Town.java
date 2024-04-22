da6c34c8daa0d25b40fdf11d44424dc7cf92e749
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-04-22 13:02:28 +0200

    Town class

---
+++ src/main/java/team5/BW5/entities/Town.java
@@ -0,0 +1,26 @@
+package team5.BW5.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "town")
+@Data
+@NoArgsConstructor
+public class Town {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "province_id")
+    private Province province;
+
+    @OneToMany(mappedBy = "town")
+    private List<Address> addressList; // importare classe address quando presente in Entities
+}
