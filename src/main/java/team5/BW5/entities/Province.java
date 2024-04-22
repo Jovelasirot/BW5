93fd39c1e10939ff928c54dae2045f19b5d0dcc0
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-04-22 12:47:55 +0200

    province class

---
+++ src/main/java/team5/BW5/entities/Province.java
@@ -0,0 +1,28 @@
+package team5.BW5.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "province")
+@Data
+@NoArgsConstructor
+public class Province {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @OneToMany
+    private List<Town> townList;
+
+    private String provinceCode;
+
+    private String region;
+
+}
