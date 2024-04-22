37b0d5718defca6a1b862c42238ce9d3b4ae15a5
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-22 22:13:46 +0200

    Update InvoiceController.java

--- src/main/java/team5/BW5/controllers/InvoiceController.java
+++ src/main/java/team5/BW5/controllers/InvoiceController.java
@@ -2,6 +2,7 @@ package team5.BW5.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +20,14 @@ public class InvoiceController {
 
     //http://localhost:3001/invoices
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN','USER')")
     public Page<Invoice> getInvoices(@RequestParam(defaultValue = "0")int page,
                                      @RequestParam(defaultValue ="15" ) int size,
                                      @RequestParam(defaultValue = "id") String sort_by){
         return this.invoiceService.getInvoices(page, size, sort_by);
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     public InvoiceRespDTO saveInvoice(@RequestBody @Validated InvoiceRequestDTO payload, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
@@ -33,10 +36,12 @@ public class InvoiceController {
         return new InvoiceRespDTO(this.invoiceService.save(payload).getId());
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void deleteInvoice(@PathVariable Long id){
         this.invoiceService.delete(id);
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     public Invoice getInvoiceById(@PathVariable Long id){
         return this.invoiceService.findById(id);
     }
